ba5986fbb091e28d4ab871a22a28e142b8924ec8
NavenRajkumar <naven1999@gmail.com>
2017-06-11 20:27:27 +0530

    Implement border length function of KMP algo

--- StringMatch.cpp
+++ StringMatch.cpp
@@ -25,16 +25,31 @@ vector<string> readPage(int pgno){
 	return lines;
 }
 
-int* prefFunc(string pat){
-	int len=pat.size();
-	int f[len];
-	int j=0;
+void prefFunc(string pat, int* f){
+	int len=pat.size(), j=0;
+	f[0]=0;
+	for(int i=1; i<len; i++){
+		while(j>=0&&(pat[j]!=pat[i])){
+			if(j-1>=0){
+				j=f[j-1];
+			}
+			else
+				j=-1;
+		}
+		j++;
+		f[i]=j;
+	}
+	return;
 }
 
 int main(){
 	vector<string> lines = readPage(2);
-	for(int i=0; i<lines.size(); i++){
-		cout<<lines[i]<<"\n";
+	string pat="hellohello";	
+	int f[pat.size()];
+	prefFunc(pat, f);
+        for(int i=0; i<pat.size(); i++){
+		cout<<f[i]<<" ";
 	}
+		
 }
 
